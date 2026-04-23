f16bd50feb6277d815e7432983d8546318fdb817
ctuck12 <ctuck12@gmail.com>
2026-04-23 08:49:00 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -412,7 +412,7 @@ export default function Page() {
               src="/logo-banner.png"
               alt="Golf Majors Pool"
               style={{
-                height: 104,
+                height: 160,
                 width: 'auto',
                 objectFit: 'contain',
                 display: 'block',
