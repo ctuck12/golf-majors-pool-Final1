a7e7c8effc3c4b974e14a2b07f8dd855a5fa8730
ctuck12 <ctuck12@gmail.com>
2026-04-22 19:15:04 -0500

    Claude v1.5

--- app/page.tsx
+++ app/page.tsx
@@ -1216,8 +1216,6 @@ export default function Page() {
                     </div>
                   </div>
                 </div>
-
-                </div>
               </div>
 
             </div>
