56a1be802cc738adb760ba4163fe3b578539ec10
ctuck12 <ctuck12@gmail.com>
2026-04-29 13:41:14 -0500

    Fix details typo

--- app/page.tsx
+++ app/page.tsx
@@ -3772,7 +3772,7 @@ export default function Page() {
                   <span style={{ color: '#43b36b', marginRight: 6 }}>🟢</span>
                   <span style={{ marginRight: 6 }}>➤</span>
                   Participants will be assigned a fixed salary cap of <strong>$50,000</strong> they must stay under in
-                  order to create their 6-player roster. Thes 6 golfers make up their player roster for that specific
+                  order to create their 6-player roster. These 6 golfers make up their player roster for that specific
                   tournament.
                 </div>
                 <div>
